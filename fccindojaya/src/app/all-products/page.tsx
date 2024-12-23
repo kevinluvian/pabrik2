57145f471e5b72e081c467668a95ab15e8f623f8
Kevin Luvian <kevinlsmak1@gmail.com>
2024-12-23 21:58:51 +0800

    Update products page

--- fccindojaya/src/app/all-products/page.tsx
+++ fccindojaya/src/app/all-products/page.tsx
@@ -1,93 +1,19 @@
-"use client";
-
 import { Footer } from "~/app/_components/footer";
 import { Header } from "~/app/_components/header";
+import { api } from "~/trpc/server";
+import { FolderArrowDownIcon } from "@heroicons/react/24/outline";
 
-const products = [
-  {
-    id: 1,
-    name: "Earthen Bottle",
-    href: "#",
-    price: "$48",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-  },
-  {
-    id: 2,
-    name: "Nomad Tumbler",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-02.jpg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
-  },
-  {
-    id: 3,
-    name: "Focus Paper Refill",
-    href: "#",
-    price: "$89",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-03.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    id: 4,
-    name: "Machined Mechanical Pencil",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-04.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  {
-    id: 5,
-    name: "Earthen Bottle",
-    href: "#",
-    price: "$48",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-01.jpg",
-    imageAlt:
-      "Tall slender porcelain bottle with natural clay textured body and cork stopper.",
-  },
-  {
-    id: 6,
-    name: "Nomad Tumbler",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-02.jpg",
-    imageAlt:
-      "Olive drab green insulated bottle with flared screw lid and flat top.",
-  },
-  {
-    id: 7,
-    name: "Focus Paper Refill",
-    href: "#",
-    price: "$89",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-03.jpg",
-    imageAlt:
-      "Person using a pen to cross a task off a productivity paper card.",
-  },
-  {
-    id: 8,
-    name: "Machined Mechanical Pencil",
-    href: "#",
-    price: "$35",
-    imageSrc:
-      "https://tailwindui.com/plus/img/ecommerce-images/category-page-04-image-card-04.jpg",
-    imageAlt:
-      "Hand holding black machined steel mechanical pencil with brass tip and top.",
-  },
-  // More products...
-];
+interface Product {
+  id: number;
+  name: string;
+  href: string;
+  imageSrc: string;
+  imageAlt: string;
+}
 
-export default function AllProducts() {
+export default async function AllProducts() {
+  const { products } = await api.product.GetProducts({ text: "from tRPC" });
+
   return (
     <div className="bg-gray-50">
       <Header />
@@ -96,10 +22,23 @@ export default function AllProducts() {
         <main>
           <div className="bg-white">
             <div className="mx-auto max-w-7xl px-4 pb-4 pt-8 sm:px-6 lg:px-8">
-              <h1 className="text-3xl font-bold tracking-tight text-gray-900">
+              <h1 className="pb-8 text-3xl font-bold tracking-tight text-gray-900">
                 Semua Produk
               </h1>
-              <p className="mt-4 max-w-xl text-sm text-gray-700"></p>
+
+              <a
+                target="_blank"
+                href="https://cdn.fccindojaya.co.id/katalog.pdf"
+                className="relative z-10 inline-block rounded-md border border-transparent bg-indigo-600 px-8 py-2 text-center text-sm text-white hover:bg-indigo-700"
+              >
+                <div className="flex items-center justify-center">
+                  <FolderArrowDownIcon
+                    aria-hidden="true"
+                    className="mr-2 size-6"
+                  />
+                  Download Katalog
+                </div>
+              </a>
             </div>
           </div>
 
@@ -136,7 +75,12 @@ export default function AllProducts() {
 
             <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
               {products.map((product) => (
-                <a key={product.id} href={product.href} className="group">
+                <a
+                  key={product.id}
+                  href={product.href}
+                  target="_blank"
+                  className="group"
+                >
                   <img
                     alt={product.imageAlt}
                     src={product.imageSrc}
--- fccindojaya/src/app/page.tsx
+++ fccindojaya/src/app/page.tsx
@@ -1,15 +1,7 @@
 import { HomeComponent } from "~/app/_components/home";
-// import { auth } from "~/server/auth";
 import { HydrateClient } from "~/trpc/server";
 
 export default async function Home() {
-  // const hello = await api.post.hello({ text: "from tRPC" });
-  // const session = await auth();
-
-  // if (session?.user) {
-  //   void api.post.getLatest.prefetch();
-  // }
-
   return (
     <HydrateClient>
       <HomeComponent></HomeComponent>
